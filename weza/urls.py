e9b5b350c8e5904e7af175e4850f0a734b1c125a
smith <smithmurichomorgan@gmail.com>
2023-03-09 10:47:32 +0300

    typo

--- weza/urls.py
+++ weza/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import QuestionList, QuestionDetail, AnswerCreate, AnswerUpdate, register, login
 
 urlpatterns = [
-path('register/', register, name='register'),
-path('login/', login, name='login'),
-path('questions/', QuestionList.as_view(), name='question_list'),
-path('questions/int:pk/', QuestionDetail.as_view(), name='question_detail'),
-path('questions/int:question_id/answers/', AnswerCreate.as_view(), name='answer_create'),
-path('answers/int:pk/', AnswerUpdate.as_view(), name='answer_update'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('questions/', QuestionList.as_view(), name='question_list'),
+    path('questions/<int:pk>/', QuestionDetail.as_view(), name='question_detail'),
+    path('questions/<int:question_id>/answers/', AnswerCreate.as_view(), name='answer_create'),
+    path('answers/<int:pk>/', AnswerUpdate.as_view(), name='answer_update'),
 ]
