ba214af68ef015c147c711a66fb31eaa57630f87
smith <smithmurichomorgan@gmail.com>
2023-03-10 11:46:38 +0300

    updated

--- weza/serializers.py
+++ weza/serializers.py
@@ -6,7 +6,10 @@ from .models import Question, Answer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
 
 
 class QuestionSerializer(serializers.ModelSerializer):
