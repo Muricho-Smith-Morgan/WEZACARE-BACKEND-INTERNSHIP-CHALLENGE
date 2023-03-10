c82dfe7f7ca41e6f53e4903dd58981864e74239a
smith <smithmurichomorgan@gmail.com>
2023-03-10 11:45:28 +0300

    signals

--- weza/apps.py
+++ weza/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class WezaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'weza'
+
+    def ready(self):
+        import weza.signals
+        
